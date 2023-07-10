26c6ff739742e4a9ab64eeee56410a6ab803b1df
Dani Rosa <danirosa.dev@gmail.com>
2023-07-10 17:14:11 -0300

    week2

---
+++ src/schemas/payments-schemas.ts
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+export const paymentsBodySchema = Joi.object<PaymentBody>({
+  ticketId: Joi.number().required(),
+  cardData: Joi.object({
+    issuer: Joi.string().required(),
+    number: Joi.number().required(),
+    name: Joi.string().required(),
+    expirationDate: Joi.string(),
+    cvv: Joi.number().required()
+  })
+});
+
+export type PaymentBody = {
+  ticketId: number;
+  cardData: {
+    issuer: string;
+    number: number;
+    name: string;
+    expirationDate: Date;
+    cvv: number;
+  };
+};
